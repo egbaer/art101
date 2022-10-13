7b4d8d45e9562d262d78e3bc3ad43b89acfd6507
egbaer <egbaer@ucsc.edu>
2022-10-12 18:01:31 -0700

    oog

---
+++ lab4/js/lab.js
@@ -0,0 +1,20 @@
+/**
+Summary.
+Description.
+@link URL
+@file defines the MyClass class.
+@author EmilyBaerMillionVillanueva.
+@since 1.0.0
+*/
+
+//Store a template for a greeting message with placeholders for where the user input will be inserted.
+//Take and store user input in separate variables. The user input should include:
+    //The user’s name
+    //The name of the person receiving the message
+    //The date
+    //Any input for a postscript section
+//Replace the placeholder text in the greeting message with the corresponding user input
+//Print a test message and ask for the user to accept or deny the message
+    //If message is denied, clear the template and begin the process again
+    //If the message is approved, continue on to the next step
+//Print the message to a pdf for the user to print out
