a2b25bf91d555df237bb993a766400637e3e7c96
egbaer <egbaer@ucsc.edu>
2022-10-17 17:00:25 -0700

    Damn Daniel

---
+++ lab5/js/lab.js
@@ -0,0 +1,13 @@
+alert('Hello World');
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 1995;
+
+age = 2022 - year;
+
+document.writeln("Make: " + make +"<br>");
+document.writeln("Model: " + model +"<br>");
+document.writeln("Color: " + color +"<br>");
+document.writeln("Year: " + year +"<br>");
+document.writeln("Age: " + age +" years<br>");
