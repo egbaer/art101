7e80891757380b8c26b0161e6e52f009c81a645f
egbaer <egbaer@ucsc.edu>
2022-11-02 22:02:00 -0700

    Doing Lab 10 core

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,31 +1,29 @@
-// Author: Me, myself, and I
-// Date Created: 11.1.1800
-// Email: XxXxD3m0nSlaught3RxxXx@gmail.com
-
-// This function takes the input of a user's name, sorts it, and prints it for the user to view
- function nameSorter(nameInput) {
-   // This asks users for input
-   var userName= nameInput;
-   console.log(userName);
-   // This splits the input into an array, so "sort" will work on it
-   var nameArray= userName.split('');
-   console.log(nameArray);
-   // The array is sorted
-   var sortedArray= nameArray.sort();
-   console.log(sortedArray);
-   // The array is joined into a string
-   var sortedName= sortedArray.join('');
-   console.log(sortedName);
-   // The sorted name is returned
+/*
+ * Author: Emily Baer <egbaer@ucsc.edu> and Hannah Hodor <hhodor@ucsc.edu>
+ * Created 2 November
+ * License: Public Domain
+ */
+ //We took our nameSorter function from Lab 7 and made a few modifications to it (made it take an arg, got rid of windowprompt) so we could effectively use it in our event Listener anon function
+ function nameSorter(inputeName){
+   var userName = inputeName;
+   var nameArray = userName.split('');
+   var sortedArray = nameArray.sort();
+   var sortedName = sortedArray.join("");
    return(sortedName);
  }
 
+//These are our variables that reference an element that already exists in our HTML.
 var buttonEl = document.getElementById("my-button");
 var outputEl = document.getElementById("output");
 
-buttonEl.addEventListener('click', function() {
-  var inputVal = document.getElementById("user-name").value;
-  var sortedNewName= nameSorter(inputVal);
-  outputEl.innerHTML =sortedNewName;
-  document.getElementById("user-name").value="";
+//This adds a sensor to our button on our webpage. It checks if it is clicked. If it is, it runs the anon function defined below
+buttonEl.addEventListener("click", function(){
+  //This stores the text typed by the user in the input tag
+  var inputEl = document.getElementById("user-name").value;
+  //This stores the results of a call to the nameSorter function using the user's input as an arg
+  var sortedName = nameSorter(inputEl);
+  //This changes the inner html of the output div to display the sortedName
+  outputEl.innerHTML = sortedName;
+  //This clears out the input tag after the button is clicked because it looks nicer
+  document.getElementById("user-name").value ="";
 });
